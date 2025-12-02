20fd90ed49a9de6f364fdc27c532e0cc47fa8106
smateusx <smateux10@gmail.com>
2025-12-02 15:20:43 -0300

    finalizei css

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   const nome= 'Maria'
   return (
     <div className="App">
       <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
+      <Frase />
       <SayMyName nome= 'Mateus'/>
       <SayMyName nome= 'João'/>
       <SayMyName nome= {nome}/>
