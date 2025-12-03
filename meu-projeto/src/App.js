42a5f903e5b4e7009d68eb67684d7dce9fb27e76
smateusx <smateux10@gmail.com>
2025-12-03 15:44:57 -0300

    if

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Eventos from './components/Eventos';
-import Form from './components/Form';
+import Condicional from './components/Condicional';
 
 function App() {
   
   return (
     <div className="App">
-      <h1>Testando eventos</h1>
-      <Eventos />
-      <Form />
+      <h1>Renderização Condicional</h1>
+      <Condicional />
     </div>
   );
 }
