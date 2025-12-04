608b4b3df058192e702c4ca8ecf2340924aacb27
smateusx <smateux10@gmail.com>
2025-12-03 23:31:02 -0300

    react router

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,19 +1,25 @@
-import { useState } from 'react';
-
-import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
-  const [nome, setNome] = useState()
 
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-      <SeuNome setNome={setNome}/>
-      <Saudacao nome = {nome}/>
-    </div>
-  );
+      <Router>
+        <NavBar />
+          <Routes>
+              <Route path="/" element={<Home />} />
+
+              <Route path="/empresa" element={<Empresa />}/>
+
+              <Route path="/contato" element={<Contato />}/>
+          </Routes>
+          <Footer />
+      </Router>
+  )
 }
 
 export default App;
