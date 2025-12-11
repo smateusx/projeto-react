065c2bed3d66ea7e5dc4194d70385c9ca70c8a24
smateusx <smateux10@gmail.com>
2025-12-11 00:36:46 -0300

    finalizei barra de navegação

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,9 +1,26 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <Navbar />
+    <Container customClass="min_height">
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/company' element={<Company />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/newproject' element={<NewProject />}/>
+      </Routes>
+    </Container>
+      <Footer />
+    </Router>
   );
 }
 
