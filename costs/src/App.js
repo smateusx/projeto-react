8008e888ddb5b5ebefe4f61cce76537d3ec35b94
smateusx <smateux10@gmail.com>
2025-12-11 01:01:28 -0300

    finalizei o footer

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -17,6 +18,7 @@ function App() {
         <Route path='/company' element={<Company />}/>
         <Route path='/contact' element={<Contact />}/>
         <Route path='/newproject' element={<NewProject />}/>
+        <Route path='/projects' element={<Projects />}/>
       </Routes>
     </Container>
       <Footer />
--- costs/src/components/layout/Navbar.js
+++ costs/src/components/layout/Navbar.js
@@ -10,14 +10,17 @@ function Navbar(){
             <Link to="/">
                 <img src={logo} alt='Costs'/>
             </Link>
-            <ul class={styles.list}>
-                <li class={styles.item}>
+            <ul className={styles.list}>
+                <li className={styles.item}>
                     <Link to="/">Home</Link>
                 </li>
-                <li class={styles.item}>
+                <li className={styles.item}>
+                    <Link to="/projects">Projetos</Link>
+                </li>
+                <li className={styles.item}>
                      <Link to="/company">Empresa</Link>
                 </li>
-                <li class={styles.item}>
+                <li className={styles.item}>
                     <Link to="/contact">Contato</Link>
                 </li>
             </ul>
