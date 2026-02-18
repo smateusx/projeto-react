0b0ae18b1398cee9d3650f524b69bf7832789296
smateusx <smateux10@gmail.com>
2026-02-18 20:09:06 -0300

    att

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -6,7 +6,7 @@ function NewProject() {
         <div className={styles.newProjectContainer}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm />
+            <ProjectForm btnText='Criar Projeto'/>
         </div>
     )
 }
--- costs/src/components/projects/ProjectForm.js
+++ costs/src/components/projects/ProjectForm.js
@@ -1,22 +1,31 @@
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+import styles from './ProjectForm.module.css'
 
-
-function ProjectForm() {
+function ProjectForm({btnText}) {
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do seu projeto"/>
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total"/>
-            </div>
-            <div>
-                <select name="categoryId">
-                    <option disabled selected>Selecione a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar projeto"/>
-            </div>
+        <form className={styles.form}>
+            <Input 
+                type='text' 
+                text='Nome do projeto' 
+                name='name' 
+                placeholder='Insira o nome do projeto'
+            />
+
+            <Input 
+                type='number' 
+                text='Orçamento do projeto' 
+                name='budget' 
+                placeholder='Insira o orçamento total'
+            />
+
+            <Select 
+                name='categoryId'
+                text='Selecione a categoria'
+            />
+            
+            <SubmitButton text={btnText}/>
         </form>
     )
 }
